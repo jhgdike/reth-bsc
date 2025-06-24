afa96710c75ddff2526478471718d10cc42663d9
dike <dike.w@nodereal.io>
2025-06-24 09:09:30 +0800

    fix

--- src/node/evm/ffi_helper.rs
+++ src/node/evm/ffi_helper.rs
@@ -1,4 +1,3 @@
-use std::error::Error;
 use std::str::FromStr;
 use crate::{
     chainspec::{bsc, BscChainSpec},
@@ -9,9 +8,8 @@ use crate::{
     system_contracts::SystemContract,
 };
 use reth_evm::{eth::EthBlockExecutionCtx, ConfigureEvm};
-use revm::{database::StateBuilder, Database};
 use std::sync::Arc;
-use alloy_consensus::{EthereumTxEnvelope, Header};
+use alloy_consensus::{EthereumTxEnvelope, Header, TxEip4844};
 use alloy_evm::block::BlockExecutor;
 use alloy_evm::{EvmFactory};
 use alloy_evm::precompiles::PrecompilesMap;
@@ -19,32 +17,29 @@ use alloy_primitives::{Bytes};
 use alloy_rlp::Decodable;
 use reth::revm::State;
 use reth_evm_ethereum::RethReceiptBuilder;
-use revm::database::DBErrorMarker;
 use revm::inspector::NoOpInspector;
+use revm::database::EmptyDB;
+use revm::database::CacheDB;
 use crate::node::evm::BscEvm;
 use crate::node::evm::executor::BscBlockExecutor;
+use reth_primitives::TransactionSigned;
+use reth_primitives_traits::SignerRecoverable;
 
 /// 创建BscBlockExecutor的完整示例
-pub fn create_bsc_block_executor(
-    db: Box<dyn Database<Error=dyn Error + Send + Sync>>,
-    header: &Header,
-) ->  BscBlockExecutor<BscEvm<State<Box<dyn Database<Error=dyn Error+Send+Sync>>>, NoOpInspector, PrecompilesMap>, Arc<BscChainSpec>, RethReceiptBuilder> {
+pub fn create_bsc_block_executor<'a>(
+    db: &'a mut State<CacheDB<EmptyDB>>,
+    header: &'a Header,
+) -> BscBlockExecutor<'a, BscEvm<&'a mut State<CacheDB<EmptyDB>>, NoOpInspector, PrecompilesMap>, Arc<BscChainSpec>, RethReceiptBuilder> {
     // 1. 创建链规范
     let chain_spec = Arc::new(BscChainSpec { inner: bsc::bsc_mainnet() });
 
     // 2. 创建EVM配置
     let evm_config = BscEvmConfig::bsc(chain_spec.clone());
 
-    // 3. 创建数据库状态
-    let _db = StateBuilder::new_with_database(db).build();
+    // 3. 创建EVM实例
+    let evm = BscEvmFactory::default().create_evm(db, evm_config.evm_env(header));
 
-    // 4. 创建EVM环境
-    // let evm_env = evm_config.evm_env(header);
-
-    // 5. 创建EVM实例
-    let evm = BscEvmFactory::default().create_evm(_db, evm_config.evm_env(header));
-
-    // 6. 创建执行上下文 todo: fix
+    // 4. 创建执行上下文
     let ctx = EthBlockExecutionCtx {
         parent_hash: header.parent_hash,
         parent_beacon_block_root: None,
@@ -52,14 +47,14 @@ pub fn create_bsc_block_executor(
         withdrawals: None,
     };
 
-    // 7. 创建收据构建器
+    // 5. 创建收据构建器
     let receipt_builder = RethReceiptBuilder::default();
 
-    // 8. 创建系统合约
+    // 6. 创建系统合约
     let system_contracts = SystemContract::new(chain_spec.clone());
 
-    // 9. 创建BscBlockExecutor
-    let mut executor = BscBlockExecutor::new(
+    // 7. 创建BscBlockExecutor
+    let executor = BscBlockExecutor::new(
         evm,
         ctx,
         chain_spec,
@@ -67,11 +62,10 @@ pub fn create_bsc_block_executor(
         system_contracts,
     );
     executor
-    // executor.execute_transaction();
 }
 
 pub fn batch_execute_transactions(
-    mut executor: BscBlockExecutor<BscEvm<State<Box<dyn Database<Error=dyn Error+Send+Sync>>>, NoOpInspector, PrecompilesMap>, Arc<BscChainSpec>, RethReceiptBuilder>,
+    mut executor: BscBlockExecutor<BscEvm<&mut State<CacheDB<EmptyDB>>, NoOpInspector, PrecompilesMap>, Arc<BscChainSpec>, RethReceiptBuilder>,
     transactions : Vec<String>,
 ) {
 
@@ -83,13 +77,36 @@ pub fn batch_execute_transactions(
                 continue;
             }
         };
-        let tx_envelop = EthereumTxEnvelope::decode(&mut &_tx_bytes[..]).unwrap();
-        // let transaction = TransactionSigned::decode(&mut &Bytes::from_str(tx_bytes)[..])?
-        //     .try_into_recovered()
-        //     .map_err(|tx| eyre::eyre!("failed to recover tx: {}", tx.tx_hash()))?;
+        
+        // 解析交易
+        let tx_envelop: alloy_consensus::EthereumTxEnvelope<TxEip4844>  = match EthereumTxEnvelope::decode(&mut &_tx_bytes[..]) {
+            Ok(envelope) => envelope,
+            Err(e) => {
+                println!("无法解码交易: {:?}", e);
+                continue;
+            }
+        };
+        
+        // 转换为TransactionSigned
+        let tx: TransactionSigned = match tx_envelop.try_into() {
+            Ok(tx) => tx,
+            Err(e) => {
+                println!("无法转换交易: {:?}", e);
+                continue;
+            }
+        };
+        
+        // 恢复签名者
+        let recovered = match tx.try_into_recovered() {
+            Ok(recovered) => recovered,
+            Err(e) => {
+                println!("无法恢复签名者: {:?}", e);
+                continue;
+            }
+        };
 
         let result = executor.execute_transaction_with_result_closure(
-            tx_envelop.into_typed_transaction(),
+            &recovered,
             |result| {
                 println!("交易执行结果: {:?}", result);
             },
@@ -102,7 +119,7 @@ pub fn batch_execute_transactions(
 }
 
 pub fn creat_block_executor_and_run(
-    db: Box<dyn Database<Error=dyn Error>>,
+    db: &mut State<CacheDB<EmptyDB>>,
     header: &Header,
     transactions : Vec<String>,
 ) {
@@ -112,29 +129,53 @@ pub fn creat_block_executor_and_run(
 
 mod tests {
     use alloy_consensus::{Header, TxLegacy};
+    use alloy_primitives::{Bytes, TxKind, U256};
     use alloy_rlp::Encodable;
+    use reth::revm::db::StateBuilder;
     use reth_chainspec::EthChainSpec;
+    use revm::database::InMemoryDB;
     use crate::chainspec::bsc::bsc_mainnet;
     use crate::node::evm::ffi_helper::{batch_execute_transactions, creat_block_executor_and_run};
     use crate::node::evm::ffi_helper::create_bsc_block_executor;
 
     #[test]
     fn test_create_bsc_block_executor() {
-        let db = Box::new(revm::database::InMemoryDB::default());
         let header = Header::default();
-        let executor = create_bsc_block_executor(db, &header);
-        assert!(executor.is_ok());
+
+        let empty_db = InMemoryDB::default();
+        let mut db = StateBuilder::new_with_database(empty_db).build();
+        let executor = create_bsc_block_executor(&mut db, &header);
         let tx = TxLegacy{
-            chain_id: bsc_mainnet().chain_id(),
+            chain_id: Option::from(bsc_mainnet().chain_id()),
             nonce: 0,
             gas_price: 0,
             gas_limit: 3000000,
-            to: None,
-            value: 0,
-            input: Vec::new(),
+            to: TxKind::default(),
+            value: U256::try_from(0).unwrap(),
+            input: Bytes::default(),
         };
         let mut buf = Vec::new();
         tx.encode(&mut buf);
-        batch_execute_transactions(executor.unwrap(), vec![buf.to_string()]);
+        batch_execute_transactions(executor, vec![buf.to_string()]);
+    }
+
+    #[test]
+    fn test_create_bsc_block_executor_and_run() {
+        let header = Header::default();
+
+        let empty_db = InMemoryDB::default();
+        let mut db = StateBuilder::new_with_database(empty_db).build();
+        let tx = TxLegacy{
+            chain_id: Option::from(bsc_mainnet().chain_id()),
+            nonce: 0,
+            gas_price: 0,
+            gas_limit: 3000000,
+            to: TxKind::default(),
+            value: U256::try_from(0).unwrap(),
+            input: Bytes::default(),
+        };
+        let mut buf = Vec::new();
+        tx.encode(&mut buf);
+        creat_block_executor_and_run(&mut db, &header, vec![buf.to_string()]);
     }
 }
